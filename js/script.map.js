f6229bb21e8a6a0dfa367b806325692b7657232f
Rex_V <102540929+RexV32@users.noreply.github.com>
2022-11-25 18:50:18 +0700

    Add files via upload

---
+++ js/script.map.js
@@ -0,0 +1,1 @@
+var map=document.querySelector(".map__frame"),mapOnjs=document.querySelector(".map__image-nojs");map.classList.remove("map__frame--nojs"),mapOnjs.classList.add("map__image-nojs--nojs");
